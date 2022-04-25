89aabef424346e6b035e3680e5b8d2be2b10b955
Joel Pick <joel.l.pick@gmail.com>
2022-04-25 07:43:06 +0100

    writing updates

--- build_vignette.R
+++ build_vignette.R
@@ -4,6 +4,9 @@ rm(list=ls())
 ## put in where the vignette is on you computer
 wd <- "/Users/joelpick/github/squid_vignette"
 
-## compile
-bookdown::render_book(wd, bookdown::gitbook(split_by = "section+number"))
 
+## compile
+bookdown::render_book(wd, bookdown::gitbook(split_by = "section+number"),new_session = TRUE)
+
+setwd("/Users/joelpick/github/squid_vignette")
+bookdown::render_book(input="02-sim_pop.Rmd",bookdown::gitbook(split_by = "section+number"),preview=TRUE,new_session = TRUE)
